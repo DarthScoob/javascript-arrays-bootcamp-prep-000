6db17d2eeb466aa0cf082f6cf5c369c85a25c655
Steve Pharris <spharris5200@gmail.com>
2018-04-26 23:40:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,8 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", 
+  "hundred grand", 
+  "kitkat", 
+  "skittles"];
+  
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
